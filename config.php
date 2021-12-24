f0ebebf2ef7f54c22c1a3c3beabc324939d2e56f
cocarroz <disc.miaw.sari@gmail.com>
2021-12-24 16:30:37 -0600

    Angelito

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+/* SERVER INFO */
+$server_ip = 'No';
+
+/* HEADER TEXT */
+$header_title = 'MiawLand;
+$header_caption = 'Bienvenido a "No" puto.';
+
+/* SOCIAL MEDIA LINKS */
+$twitter_link = 'https://twitter.com/Mynzari';
+$teamspeak_ip = 'No';
+$discord_link = 'https://discord.gg/ck3Uqtvu';
+
+/* SERVER INFO TEXT */
+$server_info_loading = 'Ojalá te muestre mis datos ...';
+$server_info_server_offline = 'Server is offline';
+
+/* FOOTER TEXT */
+$footer_imprint_link = 'https://www.youtube.com/watch?v=-i6WPgnh2sA';
+$footer_imprint_text = 'https://www.youtube.com/watch?v=kxA-EwUO_iQ';
