4e2ad297c9ea560287e0714de1b1c33cb9c354f3
Yosari <95259357+Mynzari@users.noreply.github.com>
2022-07-01 15:55:47 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <div>
       <PageHead title="Querida Amandita" description="No sé como dormiré hoy por la noche, no logro asimilar esto, ya las flores se secaron y los edificios se cayeron, nuestro amor ya no alimenta nuestro pequeño mundo que estabamos creando."/>
+    <p>"Por qué hay gente que se tuvo que intrometer en lo nuestro, me parece injusto que no dejen amar a una persona, me parece injusto que me arrebaten nuestras ilusiones y nuestros sueños."</p>
     </div>
   );
 }
